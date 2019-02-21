2739b6c9bf742c59329ffd95706d270a33c78fd4
Oran Rush <oranrush@Orans-MacBook-Pro.local>
2019-02-20 18:47:46 -0600

    Upload2

--- liri.js
+++ liri.js
@@ -1,16 +1,39 @@
 require("dotenv").config();
-
+var keys = require('./keys');
 var Spotify = require('node-spotify-api');
- 
-var spotify = new Spotify({
-  id: 035f7727034c46e2b6f58305ddc88c2b,
-  secret: b4396eb519534632b3d9ab97b8d27a69
-});
- 
-spotify.search({ type: 'Until We Rich', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
+
+var spotify = new Spotify(keys.spotify);
+
+var command = process.argv[2];
+var songName = process.argv[3];
+
+function spotifyThisSong() {
+  spotify.search({ type: 'track', query: songName }, function (err, data) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+
+    var artist = data.tracks.items[0].artists[0].name;
+    var albumName = data.tracks.items[0].album.name
+    var previewUrl = data.tracks.items[0].preview_url;
+    console.log(`artist: ${artist}`)
+    console.log(`album name: ${albumName}`) 
+    console.log(`preview url: ${previewUrl}`);
+    console.log(`song name: ${songName}`)
+  });
+}
+
+
+
+
+// // Spotify function
+function liri(command, value) {
+  if (command === "spotify-this-song") {
+    spotifyThisSong()
+  } else if (command == "movie-this") {
+    movieThis()
   }
- 
-console.log(data); 
-});
+
+}
+
+liri(command);
